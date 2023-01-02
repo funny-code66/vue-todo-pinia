13c45be99cf242284ee02082a65a4ceed80a7cfb
bright113gene <bright113gene@gmail.com>
2023-01-02 06:01:56 -0800

    chord: remove unneccessary files & comments

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <todo-app/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import { defineComponent } from 'vue';
 import TodoApp from './components/TodoApp.vue';
 export default defineComponent({
   name: 'App',
   components: {
-    // HelloWorld
     TodoApp
   }
 })
